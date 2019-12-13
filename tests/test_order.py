ac3291d7fd212852e87c45cb9e30e4ecda32ecbd
block-cat <kfx2007@outlook.com>
2019-12-13 13:16:06 +0000

    2.7

--- tests/test_order.py
+++ tests/test_order.py
@@ -14,7 +14,7 @@ class TestOrder(unittest.TestCase):
         """测试下单"""
         # 顺丰接口不允许重复下单，因此拿到结果就算通过
         res = self.sf.order.create_order("SFKD-20160219000021", u"测试公司",
-                                    u"张三", "18512345678", u"丰县", u"北京市昌平区", "15112345678")
+                                         u"张三", "18512345678", u"丰县", u"北京市昌平区", "15112345678")
         self.assertIn(res["result"], [1, 0])
 
     def test_cancel_order(self):
@@ -28,6 +28,11 @@ class TestOrder(unittest.TestCase):
         """测试订单查询接口"""
         res = self.sf.order.get_order("SFKD-20160219000021")
         self.assertIn(res["result"], [1, 0])
+
+    def test_get_router(self):
+        """测试路由信息"""
+        res = self.sf.order.get_route_info("SFKD-20160219000021")
+        self.assertEqual(res["result"], 0)
 
 
 if __name__ == "__main__":
